ca27afc6c9fa702250e57f278ebb7933202cc4a8
tapadipti <tapadipti@gmail.com>
2023-12-07 12:43:58 +0545

    first succsesful exp

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import sys
 from dvclive import Live
 
 with Live(save_dvc_exp=True) as live:
-    epochs = int(sys.argv[1])
+    epochs = int(10)
     live.log_param("epochs", epochs)
     for epoch in range(epochs):
         live.log_metric("train/accuracy", epoch + random.random())
